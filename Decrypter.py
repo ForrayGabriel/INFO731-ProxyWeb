d1e06f4bd1ddc787eead702641ae5b9e98b8a9be
Gabriel Forray <gabriel.forray@gmail.com>
2022-01-27 13:01:38 +0100

    Add comments

--- Decrypter.py
+++ Decrypter.py
@@ -1,43 +1,41 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 
+"""This program create the second proxy
+This proxy is the one exchanging data with the world wide web
+The other proxy have to send the request to 127.0.0.1:8001
+This program will take encrypted get request from the other proxy and decrypt it,
+then make the public request and encrypt and send back the result
+"""
+
 class handler(BaseHTTPRequestHandler):
 
-    """def do_CONNECT(self):
-
-        url = "https://" + self.path
-
-        #resp = requests.get(url, headers=self.headers)
-        #print("ceci est la réponse " + resp.text)
-
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-
-        message = "Hello, World!"
-        self.wfile.write(message.encode("utf-8"))"""
-
     def do_GET(self):
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        print(self.path)
-        print(self.command)
-        print(self.headers)
+        """This function is the one called when the other proxy send an ecrypted get request
+        The request is decrypted, sent into the web
+        The response is encrypted and sent back to the first proxy"""
 
-        url = self.path[1:]
+        # Get the encrypted request
+        request_encrypted = self.path[1:]
 
-        print(url)
+        # Decrypt the request
+        request_clear = request_encrypted
 
-        resp = requests.get(url, headers=self.headers)
-        #print("ceci est la réponse " + resp.text)
+        # Make the request and get the response
+        response_clear = requests.get(request_clear, headers=self.headers)
 
+        # Encrypt the response 
+        response_encrypted = response_clear.text
+
+        # Send the response code and the headers
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        self.wfile.write(resp.text.encode("utf-8"))
+        # Send back the encrypted response
+        self.wfile.write(response_encrypted.encode("utf-8"))
         self.wfile.close()
 
-
-
+# Launch the HTTP server on 127.0.0.1:8001
 with HTTPServer(("127.0.0.1", 8001), handler) as server:
     server.serve_forever()
--- Encrypter.py
+++ Encrypter.py
@@ -1,46 +1,50 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
 
+"""This program create the first proxy
+This proxy is the one exchanging data with the web browser
+The web browser proxy parameters have to be set on 127.0.0.1:8000
+This program will take any get request and encrypt it, then send it to the other proxy on 127.0.0.1:8001
+It will receive an encrypted response and decrypt it before sending it back to the web browser
+"""
+
 class handler(BaseHTTPRequestHandler):
 
-    def printMessage(self, message):
-        print(message)
-
     def do_CONNECT(self):
-
-        url = self.path
-
-        #resp = requests.get(url, headers=self.headers)
-        #print("ceci est la réponse " + resp.text)
-
-        self.send_response(200)
-        self.send_header('Content-type','text/html')
-        self.end_headers()
-
-        message = "Hello, World!"
-        self.wfile.write(message.encode("utf-8"))
+        """This function is the one called when the http server receive a CONNECT request
+        I currently don't know how to handle this kinf of requests and just pass"""
+        pass
+        
 
     def do_GET(self):
+        """This function is the one called when the http server receive a GET request 
+        It take the requested url, encrypt it and send it to the other proxy, wait for
+        a response, decrypt the response and send it back to the web browser"""
+
+        #Get the clear request
+        request_clear = self.path
+
+        #Encrypt the request
+        request_encrypted = request_clear
+    
+        #Generate the new url
+        url = "http://127.0.0.1:8001/"+ request_encrypted
+
+        # Ask the other proxy with the encrypted request and get the encrypted response
+        response_encrypted = requests.get(url, headers=self.headers)
         
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        self.printMessage("Je recois une demande :")
-        """print(self.path)
-        print(self.command)
-        print(self.headers)"""
+        #Decrypt the response
+        response_clear = response_encrypted.text
 
-        url = "http://127.0.0.1:8001/"+self.path
-
-        print(url)
-        # Ask the other proxy
-        resp = requests.get(url, headers=self.headers)
-        print("ceci est la réponse " + resp.text)
-
+        # Send the response code and the headers
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
 
-        self.wfile.write(resp.text.encode("utf-8"))
+        # Send the decrypted content
+        self.wfile.write(response_clear.encode("utf-8"))
         self.wfile.close()
 
+# Launch the HTTP server on 127.0.0.1:8000
 with HTTPServer(("127.0.0.1", 8000), handler) as server:
     server.serve_forever()
